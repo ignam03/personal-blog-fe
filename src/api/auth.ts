e49425b8f6d9ecc063b55d3ac70bcd36c4f6c95e
Ignacio M <ignafree97@gmail.com>
2024-03-06 02:27:46 -0300

    fix auth client

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,17 +2,17 @@ import { clientAxios } from "../config/clientAxios";
 import { LoginType } from "../types/loginType";
 import { RegisterType } from "../types/registerType";
 
-const API = {
-  prod: "http://3.133.143.241/api",
-  dev: "http://localhost:3000/api",
-};
+// const API = {
+//   prod: "http://3.133.143.241/api",
+//   dev: "http://localhost:3000/api",
+// };
 
 export const registerRequest = (user: RegisterType) =>
-  clientAxios.post(`${API.dev}/auth/signup`, user);
+  clientAxios.post(`/auth/signup`, user);
 export const loginRequest = (user: LoginType) =>
-  clientAxios.post(`${API.dev}/auth/login`, user);
+  clientAxios.post(`/auth/login`, user);
 export const logoutRequest = (config: { headers: { Authorization: string } }) =>
-  clientAxios.get(`${API.dev}/auth/logout`, config);
+  clientAxios.get(`/auth/logout`, config);
 export const meRequest = (config: { headers: { Authorization: string } }) =>
-  clientAxios.get(`${API.dev}/users/my-profile`, config);
-export const logout = () => clientAxios.post(`${API.dev}/auth/logout`);
+  clientAxios.get(`/users/my-profile`, config);
+export const logout = () => clientAxios.post(`/auth/logout`);
